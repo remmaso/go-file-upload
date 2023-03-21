d28f41f63c93f315e7d00b042e9a396dad626d00
remmaso <62595577+remmaso@users.noreply.github.com>
2023-03-21 19:02:13 +0000

    deleted files

--- hello.go
+++ hello.go
@@ -1,10 +1,7 @@
 package main
+ import "fmt"
 
-import "fmt"
 
-// Hello returns a greeting for the named person.
-func Hello(name string) string {
-    // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
-}
+ func main(){
+	fmt.Println("Hello, World")
+ }
